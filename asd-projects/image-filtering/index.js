9e5b12a07a27d7ff06d9dcefdac6e3deaa6a787e
LandonGamin4421 <78760115+LandonGamin4421@users.noreply.github.com>
2025-02-04 15:35:38 +0000

    image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
   
 
   // do not change the below line of code
@@ -32,18 +34,55 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterFunction){
+  for (var i = 0; i < image.length; i++){
+    for (var j = 0; j < image[i].length; j++){
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction){
+  var backgroundColor = image[0][0];
+  for (var i = 0; i < image.length; i++){
+    for (var j = 0; j < image[i].length; j++){
+      var rgbString = image[i][j];
+      if (image[i][j] !== backgroundColor){
+        var rgbNumbers = rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers);
+        image[i][j] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(num) {
+  num = (num < 0) ? 0
+    : (num > 255) ? 255
+    : num;
+  return num;
+}
 
 // TODO 3: Create reddify function
-
+function reddify(array){
+  array[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
-
+function decreaseBlue(array){
+  var result = array[BLUE] -= 50;
+  array[BLUE] = keepInBounds(result);
+}
+function increaseGreenByBlue(array){
+  var result = array[BLUE] + array[GREEN];
+  array[GREEN] = keepInBounds(result);
+}
 
 // CHALLENGE code goes below here
